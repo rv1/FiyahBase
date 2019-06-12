69d28b3705de9643c4b09cd7bfe1b497c9cf6139
Rahul Verma <rahulv@live.ca>
2019-06-12 10:51:57 -0400

    feature(iosSplashScreen): Added the icon to iOS splash screen

--- ios/FiyahBase/AppDelegate.m
+++ ios/FiyahBase/AppDelegate.m
@@ -20,7 +20,7 @@
                                                    moduleName:@"FiyahBase"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.97f green:0.97f blue:0.97f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
